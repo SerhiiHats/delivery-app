7e1f2ada2b6e140427b71d2c9620fc8f41ba9d72
Serhi Hats <gatsserv@gmail.com>
2023-06-08 02:19:01 +0300

    made work add images

--- src/pages/ShopsPage.js
+++ src/pages/ShopsPage.js
@@ -5,38 +5,13 @@ import {Link} from "react-router-dom";
 
 const ShopsPage = () => {
   const [nikUse, setNikUse] = useState(null);
-  const [product, setProduct] = useState(null);
-  // const src = require("../assets/img/best_sushi/black.jpg");
-
-  let listProducts = null;
+  const [products, setProducts] = useState(null);
 
   useEffect(()=>{
     console.log("count for constBd[0].nameRestaurant")
-    setNikUse(constBd[0].nik)
-    // setNikUse(constBd[0].product)
-  },[])
-
-  useEffect(()=>{
-    console.log("change nikUse")
-    // if(nikUse){
-    //   const tempRestaurant = constBd.filter(item => item.nik === nikUse);
-    //   console.log(tempRestaurant)
-    // }
-
-  //   tempRestaurant[0].product && console.log(tempRestaurant[0].product)
-  //
-  //   listProducts = tempRestaurant[0].product && tempRestaurant[0]?.product?.map(item =>{
-  //     console.log(item.id + " : " +item.image)
-  //
-  //
-  //     setProduct2(tempRestaurant)
-  //     // return(
-  //     //   <img key={item.id} id={item.id} width={500} src={require(`../assets/${item.image}`)} alt={item.title} title={item.description}/>
-  //     // )
-  //     // console.log(listProducts)
-  // });
-
-  },[]);
+    setNikUse(constBd[0].nik);
+    setProducts(constBd[0].product)
+    },[]);
 
   const handleCheckRestaurant = (e) =>{
     setNikUse(e.target.id);
@@ -45,20 +20,17 @@ const ShopsPage = () => {
   const listRestaurants = constBd.map(item =>{
     return(<p className={item.nik === nikUse ? "active normal" : "normal"} onClick={(e)=>handleCheckRestaurant(e)} id={item.nik} key={item.nik}>{item.nameRestaurant}</p>)
   })
-
   return (
     <div className={"shops"}>
       <div className={"containerRestaurants"}>Shops:
         {listRestaurants}
       </div>
-
       <div className={"containerProducts"}>
         {nikUse}
 
         <div>
-          {/*{listProducts &&{listProducts}}*/}
-         {/*<img width={500} src={require("../assets/img/best_sushi/black.jpg")} alt={"ddd"} title={"sdsd"}/>*/}
-          {/*<img src={"."} alt={"ddd"} title={"sdsd"}/>*/}
+         <img width={500} src={require("../assets/img/best_sushi/black.jpg")} alt={"ddd"} title={"sdsd"}/>
+          {products && products.map(item=><img key={item.id} id={item.id} width={500} src={require(`../assets/${item.image}`)} alt={item.title} title={item.description}/>)}
         </div>
 
       </div>
